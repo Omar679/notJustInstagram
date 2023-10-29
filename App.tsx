46e7f0812051e3cde59cda7a49bc5d2ff9f17a0f
Umar <omarsabiu679@gmail.com>
2023-10-29 10:35:24 +0100

    initial

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import colors from './src/theme/colors';
 import fonts from './src/theme/fonts';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Text style={{color: colors.primary, fontSize: fonts.size.xxlg}}>
         Hello World
       </Text>
+      <AntDesign name="upcircleo" size={30} color={'#000'} />
+      <Entypo name="heart" size={30} />
     </View>
   );
 };
